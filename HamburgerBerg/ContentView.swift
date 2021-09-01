e4180dbe351a7768a38ec4ca10de53f2efa13300
hnguyen94 <hoang.nguyen@moia.io>
2021-09-01 21:13:41 +0200

    Initial Commit

---
+++ HamburgerBerg/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HamburgerBerg
+//
+//  Created by Hoang Duc Nguyen on 01.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HamburgerBerg/HamburgerBergApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamburgerBergApp.swift
+//  HamburgerBerg
+//
+//  Created by Hoang Duc Nguyen on 01.09.21.
+//
+
+import SwiftUI
+
+@main
+struct HamburgerBergApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
