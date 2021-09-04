433173431a007ecb09366ea9a06a80d9dee3d526
hnguyen94 <hoang.nguyen@moia.io>
2021-09-04 22:06:32 +0200

    Remove ViewlModel

--- HamburgerBerg/ViewModels/ViewModelProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol ViewModelProtocol: AnyObject { }
-
-extension ViewModelProtocol where Self == ListViewModel {
-  static var list: ListViewModel {
-    ListViewModel()
-  }
-}
